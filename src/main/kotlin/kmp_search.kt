9a1dfef9a8c621dfd702b8c2b866c0d2d9a82892
Bradley Thompson <bradlet2@pdx.edu>
2020-04-25 21:46:24 -0700

    Add some preliminary code

--- src/main/kotlin/kmp_search.kt
+++ src/main/kotlin/kmp_search.kt
@@ -1,3 +1,24 @@
 // kmp_search.kt
 // This file holds the implementation for the Knuth-Morris-Pratt
 // String search algorithm.
+
+// Temporary literal string array for testing during implementation.
+val samples: Array<String> = arrayOf(
+    "Hello this is a test",
+    "abcabcabcabc",
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaab"
+)
+
+
+// Runs a supplied search function.
+// Search functions should return number of matches found.
+fun runSearch(function: ()->Number){
+    function()
+}
+
+
+fun main(args: Array<String>) {
+    for (sample in samples) {
+        println(sample)
+    }
+}
