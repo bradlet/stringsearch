815c556845d1edbddc1b784769df25c819eee332
Bradley Thompson <bradlet2@pdx.edu>
2020-04-25 21:33:35 -0700

    Add application plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,16 @@ version = "1.0-SNAPSHOT"
 
 plugins {
     kotlin("jvm") version "1.3.72"
+    application
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
+}
+
+application {
+    mainClassName = "Kmp_searchKt"
 }
 
 repositories {
