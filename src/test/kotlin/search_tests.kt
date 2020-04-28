d9ab5cd40dc6d957d9a4f91678c86e9108d9ec07
Bradley Thompson <bradlet2@pdx.edu>
2020-04-28 10:15:16 -0700

    Add unit tests for createPartials

--- src/test/kotlin/search_tests.kt
+++ src/test/kotlin/search_tests.kt
@@ -1,6 +1,29 @@
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class searchTests {
 
-    fun testContextLoads() {}
+    @Test
+    fun contextLoadsTest() {}
+
+    @Test
+    //Checks that createPartials handles empty input
+    fun emptyPatternPMTTest() {
+        val pattern = ""
+
+        val table: Array<Int> = createPartials(pattern)
+
+        assert(table.isEmpty())
+    }
+
+    @Test
+    //Checks that createPartials creates correct PMT based on hand calc'd val
+    fun correctPMTTest() {
+        val pattern = "ACABACACD"
+
+        val table = createPartials(pattern)
+        val correctTable: Array<Int> = arrayOf(0, 0, 1, 0, 1, 2, 3, 2, 0)
+
+        Assertions.assertArrayEquals(table, correctTable)
+    }
 }
