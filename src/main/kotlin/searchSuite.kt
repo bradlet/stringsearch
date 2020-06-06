e642c629aa19e428585d3cc5bc76a3b3c3cedb1f
Bradley Thompson <bradlet2@pdx.edu>
2020-06-06 14:59:38 -0700

    Fix test run for loop to run correct number of tests

--- src/main/kotlin/searchSuite.kt
+++ src/main/kotlin/searchSuite.kt
@@ -23,17 +23,17 @@ fun runTest(searchFn: (String, String) -> Array<Int>, setSize: Int, pattern: Str
 }
 
 fun main(args: Array<String>) {
-    val textSetSize: Int = Integer.parseInt(System.getenv("N") ?: "50000")
+    val textSetSize: Int = Integer.parseInt(System.getenv("N") ?: "100000")
     val numTestRuns: Int = Integer.parseInt(System.getenv("x") ?: "1")
     val pattern: String = System.getenv("PATTERN") ?: "aba"
     verbose = (System.getenv("verbose") ?: "false").toBoolean()
 
-    for (i in 1 until numTestRuns) {
+    for (i in 0 until numTestRuns) {
         println("Running Knuth-Morris-Pratt search (target = $pattern) on set of size $textSetSize.")
         runTest(::kmp, textSetSize, pattern)
     }
 
-    for (i in 1 until numTestRuns) {
+    for (i in 0 until numTestRuns) {
         println("Running Boyer-Moore search (target = $pattern) on set of size $textSetSize.")
         runTest(::bm, textSetSize, pattern)
     }
